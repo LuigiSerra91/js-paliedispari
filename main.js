02f76ee80cbe4d6a9d778c126d4a9161c9afc30c
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-04 14:52:35 +0200

    add_task_palindroma

--- main.js
+++ main.js
@@ -4,15 +4,32 @@ console.log('ciaoo');
 //Palidroma
 //Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
+let utente = prompt('Inserire una parola')
+console.log(utente);
+let message;
 
+function verification_word(utente) {
 
+   
+    const palindrome_word = utente.split("").reverse().join("");
+    
+    if (palindrome_word == utente) {
+        return true 
+    }
 
+    return false; 
+};
 
+let palindrome_word = verification_word(utente);
 
+if (palindrome_word) {
+    message = "La parola è palindroma"
 
+} else { 
+    message = "La parola non è palindroma"
+}
 
-
-
+alert(message);
 
 
 
@@ -22,7 +39,8 @@ console.log('ciaoo');
 
 
 //Pari e Dispari
-//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+//Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
 
 
